7a898d09bc637f21f450d933819d4431fd0b7209
mona43 <manonaa43@gmail.com>
2025-07-05 22:05:22 +0300

    ...

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -34,16 +34,6 @@ export default function Navbar() {
               <span className="text-white text-xl font-bold bg-gradient-to-r from-[var(--primary-color)] to-[var(--secondary-color)] bg-clip-text">أسهم المالية</span>
             </div>
           </div>
-          {/* زر التسجيل */}
-          <div className={`flex items-center transition-all duration-700 delay-300 ${mounted ? 'opacity-100 translate-x-0' : 'opacity-0 translate-x-10'}`}>
-            <Link
-              href="/form"
-              className="bg-gradient-to-r from-[var(--primary-color)] to-[var(--secondary-color)] text-white hover:scale-105 px-6 py-4 rounded-2xl text-sm font-bold transition-all duration-300 shadow-md hover:shadow-xl relative overflow-hidden group"
-            >
-              <span className="relative z-10">سجل الآن</span>
-              <span className="absolute inset-0 bg-white opacity-0 group-hover:opacity-20 transition-opacity duration-300"></span>
-            </Link>
-          </div>
         </div>
       </div>
     </nav>
